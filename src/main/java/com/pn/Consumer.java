6477298fd8111d20bcba8ef87eecb0e55decb6e9
phannguyen020501 <phannguyen147852@gmail.com>
2022-03-15 14:47:14 +0700

    Initial commit

---
+++ src/main/java/com/pn/Consumer.java
@@ -0,0 +1,48 @@
+package com.pn;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer {
+    private final static String  QUEUE_NAME = "queue_1";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        System.out.println("create a ConnectionFactory");
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        System.out.println("create a connection");
+        System.out.println("create a channel");
+        Connection connection =factory.newConnection();
+
+        Channel channel = connection.createChannel();
+
+        System.out.println("create a queue "+ QUEUE_NAME);
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        // tương tự producer: tạo 1 queue để lưu trữ các message
+        System.out.println("waiting for message. to exit press Ctrl + C");
+
+        System.out.println("start receiving messages...");
+        DeliverCallback deliverCallback = (consumerTag, delivery)-> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println("received: " + message + " ");
+        };
+
+        CancelCallback cancelCallback = consumerTag -> {};
+        String consumerTag = channel.basicConsume(QUEUE_NAME, true, deliverCallback, cancelCallback);
+        //nhận message từ queue. các đối số
+        /*String queue: tên của queue muốn nhận message từ
+        - bool autoAck: tự động gửi lại 1 ack message đến rabbitmq để báo rằng có 1 message đã dc consumer nhận
+        xử lý và rabbit có thể xóa nó
+        - DeliverCallBack: cung cấp 1 callback để xử lý khi một message đến
+        - CancelCallBack: cung cấp 1 callback để xử lý khi một consumer bị cancel vì 1 lý do nào đó, chẳng hạn
+        queue bị cancel
+         */
+        System.out.println("consumerTag: " + consumerTag);
+
+
+    }
+}
---
+++ src/main/java/com/pn/Producer.java
@@ -0,0 +1,66 @@
+package com.pn;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+    private final static String QUEUE_NAME = "queue_1";
+
+    public static void main(String[] args) {
+        System.out.println("create a connectionFactory");
+        ConnectionFactory factory = new ConnectionFactory();
+
+        factory.setHost("localhost");
+
+        System.out.println("create a connection");
+        System.out.println("create a channel");
+        try (Connection connection = factory.newConnection()){
+            Channel channel = connection.createChannel();
+            System.out.println("create queue "+ QUEUE_NAME);
+            channel.queueDeclare(QUEUE_NAME,false, false, false, null);
+            //tạo một queue để lưu trữ các message, các đối số
+            /*
+            * String name: tên của queue muốn gửi message đến
+            * bool durable: queue và message sẽ bị xóa khi rabbitmq server stop, để giữ lại thì set true
+            * bool exclusive: queue sẽ bị xóa khi connection close
+            * book autoDelete: queue sẽ bị xóa khi consumer cuối cùng của nó bị hủy hoặc đóng hoặc mất kết nối
+            * map<String, object> argumens: các tham số nếu có
+            * */
+
+            System.out.println("start sending message");
+            try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+                String message;
+                do {
+                    System.out.println("Enter message: ");
+                    message = br.readLine().trim();
+                    channel.basicPublish("", QUEUE_NAME, null,
+                            message.getBytes(StandardCharsets.UTF_8));
+                    // gửi message lên exchange
+                    /*
+                    * String exchange: tên exchange: không có là empty-> default exchange
+                    * String routingKey: key của exchange
+                    * basicProperties: props: các thuộc tính khác cho message
+                    * byte[] body: nội dụng của tin nhắn
+                    * */
+                    System.out.println("Sent: " + message+ "'");
+
+                }while (!message.equalsIgnoreCase("close"));
+            }finally {
+                System.out.println("close connection and free resources");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
